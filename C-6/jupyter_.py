cc74f799b3e454a5750498ad2e9ed5de1089c742
Donnahue George <Donnahuegjr@gmail.com>
2021-06-17 15:06:06 -0400

    adding pandas lesson

---
+++ C-6/jupyter_.py
@@ -0,0 +1,17 @@
+"""
+What is the jupyter notebook?
+
+How to access jupyter notebook
+
+
+Shortcuts
+Enter == newline in same cell
+Ctrl + Enter == execute command
+Alt + Enter == new cell
+Shift Enter == execute and create new cell
+dd = delete cell
+
+
+
+data with no headers== header = None
+"""
